50ba170b145f2a674af0aa933c55f3866c26c2fc
justyamzal <justyamzal@gmail.com>
2025-04-12 16:24:10 +0700

    adding math.js

---
+++ JS_SSD/05-exercise-variable.js
@@ -0,0 +1,98 @@
+// ----- Lesson 5 Exercise - Variables ----- //
+
+//* --- 5a. 
+let name = 'yamzal';
+// console.log(name);
+
+// const Name = 'yamzal';
+// console.log(Name);
+
+//* --- 5b.
+// const Name = 'yamzal';
+// console.log(`My name is ${Name}`);
+
+
+//* --- 5c.
+const coffee = 5;
+const bagels = 3;
+const soup = 9;
+
+// const cost = coffee + 2 * bagels + soup;
+// console.log(`total cost is : $${cost}`);
+
+//* --- 5d.
+// console.log(`cost of food: $${cost}`);
+
+//* --- 5e.
+//! restaurant charges a 10% tax (multiply 0.1)
+// const tax = 0.1;
+// const cost = coffee + 2 * bagels + soup;
+// const taxCost = cost * tax;
+// const costAfterTax = cost - 2;
+
+
+// console.log(`cost of food $${cost}`)
+// console.log(`tax for the food is $${taxCost}`);
+// console.log(`cost after got tax is $${costAfterTax}`);
+
+//* --- 5f.
+// const cost = coffee + 2 * bagels + soup;
+// const tax = 0.1;
+// const taxCost = cost * tax;
+//! tax => taxCost
+// console.log(`tax for the food is $${taxCost}`);
+
+//* --- 5g.
+
+const cost = coffee + 2 * bagels + soup;
+// console.log(`Cost of food :  $${cost}`);
+
+const tax = 0.1 * cost; 
+// console.log(`Tax(10%): $${tax}`);
+
+const totalCost = cost + tax;
+// console.log(`total cost: $${totalCost}`);
+
+//* --- 5h.
+//! done in cartQuantity page html
+
+
+//* --- 5i.
+//! done in cartQuantity page html
+
+//* --- 5j.
+//! done in cartQuantity page html
+
+
+//* --- 5k.
+//! done in cartQuantity page html
+
+//* --- 5l.
+//! done in calculator page html
+
+//* --- 5m.
+//! done in calculator page html
+
+//* --- 5n.
+//! done in calculator page html
+
+//* --- 5o.
+//! done in calculator page html
+
+
+//* --- 5p.
+//! done in calculator page html
+
+
+//* --- 5q.
+//! done in calculator page html
+
+
+//* --- 5r.
+//! done in calculator page html
+
+
+
+
+
+

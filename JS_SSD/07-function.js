d0f9323a295b8fa720ae9f3dfb450ca92515193e
justyamzal <justyamzal@gmail.com>
2025-04-15 01:13:58 +0700

    adding 07

---
+++ JS_SSD/07-function.js
@@ -0,0 +1,36 @@
+/*
+In this lesson:
+1. functions = let us reuse code
+2. return = get a value out a function
+3. parameters = put values into a function
+4. improved the code for rock, paper, scissors
+*/
+
+/* Rules for function names : 
+1. can't use special words
+example: function
+2. can't starts with a number 
+3. can't use special characters
+except: $_
+
+best practice = use camelCase
+*/
+
+function function1() {
+    // console.log('hello');
+    // console.log(2 + 2);
+
+}
+// function1();
+
+function calculateTax(parameters1) {
+    // console.log(parameters1 * 0.1);
+}
+calculateTax(2000);
+calculateTax(5000);
+
+function calculateTax_(cost, taxPercent) {
+    // console.log(cost * taxPercent);
+}
+calculateTax_(2000, 0.2);
+calculateTax_(5000, 0.2);
